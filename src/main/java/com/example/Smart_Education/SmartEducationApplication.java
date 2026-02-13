beb1034492c3aeda6b8535de656f58ff4d4394b7
Nilesh-creator-code <nileshvishwakarma32142@gmail.com>
2026-02-13 09:44:19 +0530

    Initial commit - Internship Management System

---
+++ src/main/java/com/example/Smart_Education/SmartEducationApplication.java
@@ -0,0 +1,13 @@
+package com.example.Smart_Education;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartEducationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartEducationApplication.class, args);
+	}
+
+}
